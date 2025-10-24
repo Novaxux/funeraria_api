48d7ecd2fd401580afb5e7baca4c92c08f915b00
SEBASTIANTORRESL0K1 <svelasco13@ucol.mx>
2025-10-24 09:59:04 -0600

    modificando controllers

--- models/auth.repository.js
+++
@@ -1,6 +0,0 @@
-import { userPool as pool } from '../config/db.js';
-
-export const login = async (table, email, password, idField) => {
-    const [rows] = await pool.query(`SELECT * FROM ${table} WHERE email = ? AND password = ?`, [email, password]);
-    return rows[0];
-};
